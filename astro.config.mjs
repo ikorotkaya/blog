8543705e5f2ce22481eb581b8c9132c33f130e32
ikorotkaya <irinkakorotkaya@gmail.com>
2024-05-01 14:35:49 +0200

    Add base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind({})],
   output: 'static',
-  outDir: './docs'
+  outDir: './docs',
+  base: 'personal-blog/'
 });
