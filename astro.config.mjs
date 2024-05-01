7b850ac184addbb62796ef5a2ba9fdd8569d76bb
ikorotkaya <irinkakorotkaya@gmail.com>
2024-05-01 14:17:48 +0200

    Delete deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({})],
-  site: 'https://korotkaya-blog.github.io'
+  integrations: [tailwind({})]
 });
